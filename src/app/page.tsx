93648a6af1214114bb1211dcd98475fdc7832c06
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-08-26 11:47:44 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Stats from "@/components/Home/Stats";
 export default function Home() {
   return (
    
-    <main className="min-h-screen bg-gradient-to-br from-primary/35 to-secondary/20">
+    <main className="min-h-screen bg-gradient-to-br from-primary/5 to-secondary/2 ">
       <section className="w-full h-screen">
         <Hero1 />
       </section>
