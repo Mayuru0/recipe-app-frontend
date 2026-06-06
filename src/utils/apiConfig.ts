a4c0ad919c8263873e814f2c45211de8aa9108cf
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2026-06-06 12:53:06 +0530

    bug fix

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,5 +1,4 @@
 
 //export const BASE_URL="http://localhost:8080";
 //export const BASE_URL="https://recipebackend.giize.com";
-
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://recipe-app-backend-olive.vercel.app";
+export const BASE_URL="https://recipe-app-backend-olive.vercel.app";
